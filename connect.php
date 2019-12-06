9758e11a4a6ce9445afafabe9f01407b6720949e
Ines <ines@windows>
2019-12-06 06:17:31 +0100

    corecction php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	try {				
-		$bdd_site = new PDO("mysql:host=51.83.253.234;dbname=nuitdelinfo", "root", "root38000");					
+		$bdd_site = new PDO("mysql:host=localhost;dbname=nuitdelinfo", "root", "root38000");					
 	} catch (Exception $error) {						
 		print('Une erreur est survenue : ' . $error->getMessage());						
 	}
